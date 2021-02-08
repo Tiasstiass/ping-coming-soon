f894d08022f89417831fb101a3b912622f8efbc4
Tiasstiass <m.moutte2@gmail.com>
2021-02-08 22:40:20 +0800

    secondcommit

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,11 @@ button.addEventListener("click", (e) => {
 
 function ValidateEmail(inputText) {
   let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (inputText.value.match(mailformat)) {
+
+  if (input.value === "") {
+    error.textContent = "Whoops! It looks like you forgot to add your email";
+    error.classList.remove(`hidden`);
+  } else if (inputText.value.match(mailformat)) {
     input.classList.contains(`border-lightRed`)
       ? input.classList.replace(`border-lightRed`, `border-green`)
       : input.classList.replace(`border-paleBlue`, `border-green`);
@@ -18,11 +22,12 @@ function ValidateEmail(inputText) {
       input.classList.replace(`border-green`, `border-paleBlue`);
       error.classList.add("hidden");
     }, 2000);
+    error.classList.remove(`hidden`);
     input.value = "";
     error.classList.replace(`text-lightRed`, `text-green`);
     error.textContent = "Thank you for subscribing :)";
-    error.classList.remove(`hidden`);
   } else {
+    error.textContent = "Please provide a valid e-mail adress";
     input.classList.replace(`border-paleBlue`, `border-lightRed`);
     error.classList.remove(`hidden`);
   }
